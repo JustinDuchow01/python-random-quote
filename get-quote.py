968ac27712f7bf017dc291be173cc10f0b711385
Justin <justin-duchow@web.de>
2020-02-21 16:45:53 +0100

    Random quote is working

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,13 @@
+import random
 def primary():
-   print("Keep it logically awesome.")
 
+   print("Keep it logically awesome.")
+   rdn = random.randint(0, 13)
+   f = open("quotes.txt")
+   quotes = f.readlines()
+   f.close()
+
+   print(quotes[rdn])
   #f = open("quotes.txt")
   #quotes = f.readlines()
   #f.close()
